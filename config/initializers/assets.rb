4bdfff5d5b4abfa5e54477706bf8860cea5695e2
shakyeah <dshakya@Darshans-MacBook-Pro.local>
2014-11-25 07:59:40 -0500

    changed glyphicons fonts/glphicon to assets/glyphicons and it works.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,6 +11,7 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( fonts/glyphicons-halflings-regular.svg )
 # Rails.application.config.assets.precompile += %w( fonts/glyphicons-halflings-regular.ttf )
 # Rails.application.config.assets.precompile += %w( fonts/glyphicons-halflings-regular.woff )
+Rails.application.config.assets.precompile += %w( bootstrap.css )
 Rails.application.config.assets.precompile += %w( bootstrap.min.css )
 Rails.application.config.assets.precompile += %w( bootstrap-theme.css )
 Rails.application.config.assets.precompile += %w( fonts/* )
