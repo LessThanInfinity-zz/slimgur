551057bb4b3309da41be294bcdf47fa97aadd30c
shakyeah <dshakya@Darshans-MacBook-Pro.local>
2014-11-26 16:47:03 -0500

    added some progress bar CSS. Have to float divs in the stats-div. Switching to backend for time being: integrating users show page

--- app/assets/javascripts/controllers/post_controller.js
+++ app/assets/javascripts/controllers/post_controller.js
@@ -1,7 +1,22 @@
 Slimgur.PostController = Ember.ObjectController.extend({
 	testProperty: "heluuursss",
+	progressBarWidth: null,
+	popularity: function(){
+		var that = this;
+		var percent= ( that.get('numUpvotes')/(that.get('numUpvotes')+that.get('numDownvotes')) ) * 100;
+		// var progressBarStyle= "width: "+ percent.toString() + "%;";
+		// debugger
 
-	
+		if (isNaN(percent)){
+			return "width: 0%"
+		} else {
+			return "width: "+ percent.toString() + "%;";
+		}
+
+		// debugger
+		// that.set('progressBarWidth', progressBarStyle);
+
+	}.property('numUpvotes','numDownvotes'),
 	// uploadedBy: function(){
 	// 	return this.store.find
 	// }
