57dbeb7432f7778be363856de0e6a782d103715c
shakyeah <dshakya@Darshans-MacBook-Pro.local>
2014-11-26 16:58:09 -0500

    added resource user show. Nothing done on devise yet.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+		before_action :set_user
+		respond_to :html, :json
+
+  def show
+    respond_with(@user) do |format|
+      format.html
+      format.json {render json: @user}
+    end
+    # respond_with(@user)
+  end
+
+	  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def post_params
+      params[:user]
+      params.require(:user).permit(:email, :username)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   end
 
   resources :comments
+  resources :users, only: [:show]
 
   get 'static/index'
 
