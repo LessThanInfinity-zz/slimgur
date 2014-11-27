de1a02376b54c5e93d09303198399676a8581c55
shakyeah <dshakya@Darshans-MacBook-Pro.local>
2014-11-27 12:38:33 -0500

    popularity bar backend fixed

--- app/assets/javascripts/controllers/post_controller.js
+++ app/assets/javascripts/controllers/post_controller.js
@@ -1,14 +1,20 @@
 Slimgur.PostController = Ember.ObjectController.extend({
-	testProperty: "heluuursss",
+	percentValue: 100,
 	progressBarWidth: null,
+
+	numPoints: function(){
+		var that = this;
+		return that.get('numUpvotes') - that.get('numDownvotes');
+	}.property('numUpvotes', 'numDownvotes'),
+
 	popularity: function(){
 		var that = this;
 		var percent= ( that.get('numUpvotes')/(that.get('numUpvotes')+that.get('numDownvotes')) ) * 100;
 		// var progressBarStyle= "width: "+ percent.toString() + "%;";
 		// debugger
-
+		that.set('percentValue', percent);
 		if (isNaN(percent)){
-			return "width: 0%"
+			return "width: 100%"
 		} else {
 			return "width: "+ percent.toString() + "%;";
 		}
