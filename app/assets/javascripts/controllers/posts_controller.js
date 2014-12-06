9a0ec7dce6058ece3b16d61d373b62553bb72e9f
shakyeah <dshakya@Darshans-MacBook-Pro.local>
2014-12-06 07:57:36 -0500

    navigation button updates

--- app/assets/javascripts/controllers/posts_controller.js
+++ app/assets/javascripts/controllers/posts_controller.js
@@ -1,3 +1,13 @@
 Slimgur.PostsController = Ember.ArrayController.extend({
 	testProperty: "This is a test",
+	sortProperties: ['popularity'],
+	sortAscending: true,
+
+	actions: {
+		sort: function(sortBy){
+			var that = this;
+			console.log('sortPopularity fired.');
+			that.set('sortProperties', [sortBy]);
+		}
+	}
 })
