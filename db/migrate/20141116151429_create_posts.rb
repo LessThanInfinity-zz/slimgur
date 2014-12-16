fad1346930517b19a7b3423d888faf4c3fccb2d5
shakyeah <dshakya@Darshans-MacBook-Pro.local>
2014-12-16 07:01:37 -0500

    attempting changed migrations to up and down and changed drop tables

--- db/migrate/20141116151429_create_posts.rb
+++ db/migrate/20141116151429_create_posts.rb
@@ -1,14 +1,6 @@
 class CreatePosts < ActiveRecord::Migration
-  # def change
-  #   create_table :posts do |t|
-  #     t.string :title,              null: false, default: ""
-  #     t.text :description, null: false, default: ""
-
-  #     t.timestamps
-  #   end
-  # end
-
-  def up
+  def change
+    drop_table 'posts' if ActiveRecord::Base.connection.table_exists? 'posts'
     create_table :posts do |t|
       t.string :title,              null: false, default: ""
       t.text :description, null: false, default: ""
@@ -16,8 +8,31 @@ class CreatePosts < ActiveRecord::Migration
       t.timestamps
     end
   end
-
-  def down
-  	drop_table :posts
-  end
 end
+
+
+
+
+# class CreatePosts < ActiveRecord::Migration
+#   # def change
+#   #   create_table :posts do |t|
+#   #     t.string :title,              null: false, default: ""
+#   #     t.text :description, null: false, default: ""
+
+#   #     t.timestamps
+#   #   end
+#   # end
+
+#   def up
+#     create_table :posts do |t|
+#       t.string :title,              null: false, default: ""
+#       t.text :description, null: false, default: ""
+
+#       t.timestamps
+#     end
+#   end
+
+#   def down
+#   	drop_table :posts
+#   end
+# end
