4f555976e15af2fa2e3627b86f57e0b3af65574c
shakyeah <dshakya@Darshans-MacBook-Pro.local>
2014-12-16 06:52:52 -0500

    attempting changed migrations to up and down

--- db/migrate/20141116151429_create_posts.rb
+++ db/migrate/20141116151429_create_posts.rb
@@ -1,10 +1,23 @@
 class CreatePosts < ActiveRecord::Migration
-  def change
+  # def change
+  #   create_table :posts do |t|
+  #     t.string :title,              null: false, default: ""
+  #     t.text :description, null: false, default: ""
+
+  #     t.timestamps
+  #   end
+  # end
+
+  def up
     create_table :posts do |t|
       t.string :title,              null: false, default: ""
       t.text :description, null: false, default: ""
 
       t.timestamps
     end
+  end
+
+  def down
+  	drop_table :posts
   end
 end
