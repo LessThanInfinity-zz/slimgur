57af191d488567c583471020c775bd18d9615e97
shakyeah <dshakya@Darshans-MacBook-Pro.local>
2014-12-29 10:55:11 -0500

    re-add individual assets compilation fonts

--- app/assets/javascripts/controllers/new_post_controller.js
+++ app/assets/javascripts/controllers/new_post_controller.js
@@ -1,19 +1,3 @@
-// Slimgur.NewController = Ember.Controller.extend({
-
-//   actions: {
-//     createPost: function() {
-//     	console.log('createPost fired');
-//     	// debugger
-//       var self = this;
-//       var post = this.store.createRecord('post', this.get('fields'));
-//       post.save().then(function() {
-//         self.transitionToRoute('post', post);
-//       });
-//     }
-//   }
-
-// });
-
 Slimgur.PostsNewController = Ember.Controller.extend({
   actions: {
     createPost: function() {
