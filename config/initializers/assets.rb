ce09841b37d0fe03ef13d3e40fe113100221d877
shakyeah <dshakya@Darshans-MacBook-Pro.local>
2014-12-29 11:20:47 -0500

    set compilation true in config

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,12 +7,12 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( fonts/glyphicons-halflings-regular.eot )
-Rails.application.config.assets.precompile += %w( fonts/glyphicons-halflings-regular.svg )
-Rails.application.config.assets.precompile += %w( fonts/glyphicons-halflings-regular.ttf )
-Rails.application.config.assets.precompile += %w( fonts/glyphicons-halflings-regular.woff )
+# Rails.application.config.assets.precompile += %w( fonts/glyphicons-halflings-regular.eot )
+# Rails.application.config.assets.precompile += %w( fonts/glyphicons-halflings-regular.svg )
+# Rails.application.config.assets.precompile += %w( fonts/glyphicons-halflings-regular.ttf )
+# Rails.application.config.assets.precompile += %w( fonts/glyphicons-halflings-regular.woff )
 Rails.application.config.assets.precompile += %w( bootstrap.css )
 # Rails.application.config.assets.precompile += %w( bootstrap.min.css )
 Rails.application.config.assets.precompile += %w( bootstrap-theme.css )
-# Rails.application.config.assets.precompile += %w( fonts/* )
+Rails.application.config.assets.precompile += %w( fonts/* )
 # Rails.application.config.assets.precompile += %w( stylesheets/* )
